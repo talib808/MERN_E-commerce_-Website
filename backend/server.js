cec788d5e5c5855ff4b344249901899447f1b4bc
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2023-03-22 21:56:19 +0530

    Final submission

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 9000;
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({ path: "config/config.env" });
-}
+ }
 
 const server = app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
